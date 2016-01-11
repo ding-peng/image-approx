b3248b729b532f3f2741d663612610634dfbceb2
Ding Peng <294420321@qq.com>
2016-01-11 17:56:51 -0500

    add comments

--- evo.py
+++ evo.py
@@ -10,21 +10,38 @@ from PIL import Image, ImageDraw, ImageChops
 
 from genetic import genetic, gene
 
-TARGET_IMG_NAME = 'test.jpg'
-POP_SIZE = 25
-MUT_RATE = 0.05
-GENERATIONS = 100000
-CHILDREN_PER_GEN = 5
-TRI_NUM = 100
-BACKGROUND_COLOR = 'white'
+TARGET_IMG_NAME = 'test.jpg'  # target image file name
+POP_SIZE = 25                 # population size
+MUT_RATE = 0.05               # mutation rate
+GENERATIONS = 100000          # number of generations
+CHILDREN_PER_GEN = 5          # children generated in each generations
+TRI_NUM = 100                 # number of triangles
+BACKGROUND_COLOR = 'white'    # background color
 
+# load target image and resize
 TARGET = Image.open(TARGET_IMG_NAME).convert('RGB')
 TARGET = TARGET.resize((500, 500))
 
 
-class individual:
+class individual(object):
+    """
+    The individual class, which is an image.
 
+    Attributes:
+        genes: all the genes(triangles).
+        im: the actual image file.
+        fitness: the fitness, the lower the better
+    """
     def __init__(self, parent_1=None, parent_2=None):
+        """
+        Inits an individual.
+        If it has parents, generate its genes via crossover operation.
+        If not, generate its genes randomly.
+
+        Args:
+            parent_1: a parent.
+            parent_2: another parent.
+        """
         self.genes = []
         op = genetic()
         if parent_1 and parent_2:
@@ -33,10 +50,20 @@ class individual:
         else:
             for i in xrange(TRI_NUM):
                 self.genes.append(gene())
+        # set actual image
         self.im = self.get_current_img()
+        # calculate fitness
         self.fitness = self.get_fitness(TARGET)
 
     def get_current_img(self):
+        """
+        Get actual image file using the properties of the triangles.
+
+        Important: set canvas to RGB, draw object to RGBA.
+
+        Returns:
+            im: the image file.
+        """
         im = Image.new('RGB', (500, 500), BACKGROUND_COLOR)
         draw = ImageDraw.Draw(im, 'RGBA')
         for gene in self.genes:
@@ -47,9 +74,25 @@ class individual:
         return im
 
     def save_current_img(self, f_name):
+        """
+        Save image to a file.
+
+        Args:
+            f_name: saved file name.
+        """
         self.im.save(f_name, 'PNG')
 
     def get_fitness(self, target):
+        """
+        Get fitness of the individual.
+        This was calculated by RMS distance of histograms.
+
+        Args:
+            target: target image.
+
+        Returns:
+            the fitness value.
+        """
         h = ImageChops.difference(target, self.im).histogram()
         return math.sqrt(reduce(operator.add,
                          map(lambda h, i: h*(i**2),
@@ -57,6 +100,12 @@ class individual:
                             (float(target.size[0]) * target.size[1]))
 
     def get_array(self):
+        """
+        Get array representation(DNA sequence) of the genes.
+
+        Returns:
+            array: DNA sequence.
+        """
         array = []
         for g in self.genes:
             array.append(g.pos_1)
@@ -68,6 +117,14 @@ class individual:
         return array
 
     def generate_genes_from_array_with_mut(self, array, rate):
+        """
+        Transform DNA sequence into a list of genes,
+        plus a mutation.
+
+        Args:
+            array: DNA sequence.
+            rate: mutation rate.
+        """
         new_array = zip(*[iter(array)]*6)
         self.genes = []
         for chunk in new_array:
@@ -82,7 +139,16 @@ class individual:
             self.genes.append(g)
 
 
-def initalize(pop):
+def initialize(pop):
+    """
+    Inits population.
+
+    Args:
+        pop: population.
+
+    Returns:
+        pop: population.
+    """
     for i in xrange(POP_SIZE*2):
         pop.append(individual())
     pop.sort(key=lambda x: x.fitness)
@@ -90,11 +156,20 @@ def initalize(pop):
     return pop
 
 
-def evolove(pop):
+def evolve(pop):
+    """
+    Evolve population.
+
+    Args:
+        pop: population.
+
+    Returns:
+        pop: population.
+    """
     for i in xrange(GENERATIONS):
 
         children = []
-
+        # generate weighed choices according to fitness
         parent_choices = []
         w = 100
         for p in pop:
@@ -102,20 +177,21 @@ def evolove(pop):
             if w > 0:
                 w = w - 10
         pop_choices = [val for val, cnt in parent_choices for j in range(cnt)]
-
+        # generate children
         for j in range(CHILDREN_PER_GEN):
             parent_1 = random.choice(pop_choices)
             parent_2 = random.choice(pop_choices)
             child = individual(parent_1, parent_2)
             children.append(child)
-
+        # compare and save new individuals
         pop += children
         pop.sort(key=lambda x: x.fitness)
         pop = pop[:POP_SIZE]
-
+        # print log info
         if i % 10000 == 0 or i in [100, 200, 500, 1000, 5000]:
-            pop[0].save_current_img(str(i)+'_b.png')
+            pop[0].save_current_img(str(i)+'_b.png')  # save intermediate imgs
         if i % 10 == 0:
+            # print current best fitness and avg fitness
             avg = sum(map(lambda x: x.fitness, pop)) / 25
             print "Finish " + str(i), pop[0].fitness, avg
     return pop
@@ -123,7 +199,7 @@ def evolove(pop):
 
 if __name__ == '__main__':
     pop = []
-    pop = initalize(pop)
-    pop = evolove(pop)
+    pop = initialize(pop)
+    pop = evolve(pop)
     pop[0].save_current_img('best.png')
     print pop[0].fitness
--- genetic.py
+++ genetic.py
@@ -6,12 +6,26 @@
 import random
 
 
-class gene:
+class gene(object):
+    """
+    The gene class, which represent a triangle.
 
+    Attributes:
+        pos_1: The first vertex of the triangle.
+        pos_2: The second vertex of the triangle.
+        pos_2: The third vertex of the triangle.
+        color: The GRBA value of the triangle.
+    """
     def __init__(self):
+        """
+        Inits a gene with mutation operation.
+        """
         self.mutate()
 
     def mutate(self):
+        """
+        Mutation operation. Change the values of a gene.
+        """
         self.pos_1 = (random.randint(0, 500), random.randint(0, 500))
         self.pos_2 = (random.randint(0, 500), random.randint(0, 500))
         self.pos_3 = (random.randint(0, 500), random.randint(0, 500))
@@ -21,24 +35,66 @@ class gene:
                       'a': 128}
 
 
-class genetic:
-
+class genetic(object):
+    """
+    The genetic operation utils class.
+    """
     def crossover(self, parent_1, parent_2, rate):
+        """
+        Crossover method 1, one-point crossover. (legacy)
+        Generate a child with a portion of complete genes from parent_1
+        and another portion of complete genes from parent_2.
+
+        Args:
+            parent_1: a parent.
+            parent_2: another parent.
+            rate: the portion of genes that come from parent_1.
+
+        Returns:
+            new_genes: a list of new genes.
+        """
         gene_num = len(parent_1.genes)
         mid = int(gene_num * rate)
         new_genes = parent_1.genes[:mid] + parent_2.genes[mid:]
         return new_genes
 
     def crossover2(self, parent_1, parent_2, tri_num):
+        """
+        Crossover method 2, uniform crossover. (legacy)
+        Generate a child with some complete genes from parent_1
+        and some complete genes from parent_2.
+
+        Args:
+            parent_1: a parent.
+            parent_2: another parent.
+            tri_num: number of triangles that form the result image.
+
+        Returns:
+            new_genes: a list of new genes.
+        """
         new_genes = []
         for i in xrange(tri_num):
             if random.uniform(0, 1) < 0.5:
-                new_genes.append(parent_1.genes[i])
+                new_genes.append(parent_1.genes[i])  # get gene from parent_1
             else:
-                new_genes.append(parent_2.genes[i])
+                new_genes.append(parent_2.genes[i])  # get gene from parent_2
         return new_genes
 
     def crossover3(self, parent_1, parent_2, tri_num):
+        """
+        Crossover method 3, uniform crossover. (currently used)
+        Transform the parent genes into array representation(DNA sequence).
+        The child get a portion of DNA from parent_1, then from parent_2,
+        then from parent_1 again, and so on.
+
+        Args:
+            parent_1: a parent.
+            parent_2: another parent.
+            tri_num: number of triangles that form the result image.
+
+        Returns:
+            new_genes: an array representation(DNA sequence) of the gene.
+        """
         array1 = parent_1.get_array()
         array2 = parent_2.get_array()
         new_array = []
@@ -56,6 +112,18 @@ class genetic:
         return new_array
 
     def mutate(self, genes, rate, tri_num):
+        """
+        Mutation Selector:
+        Select some genes and let them mutate.
+
+        Args:
+            genes: the list of genes of an individual.
+            rate: the mutation rate.
+            tri_num: number of triangles that form the result image.
+
+        Returns:
+            genes: a list of mutated genes.
+        """
         if random.uniform(0, 1) < rate:
             mut_genes = random.sample(range(tri_num), 5)
             for g in mut_genes:
